24ef2c57dd5b900450da14585e8aa9a08f9c732d
Adrian <ad.penchev@gmail.com>
2022-09-28 22:15:36 -0400

    Set up HashMap with two Strings

--- part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -5,6 +5,11 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations required here!
+        HashMap<String, String> nickNames = new HashMap<>();
+        nickNames.put("matthew","matt");
+        nickNames.put("michael","mix");
+        nickNames.put("arthur","artie");
+        System.out.println(nickNames.get("matthew"));
     }
 
 }
