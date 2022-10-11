4422fa09115ed0b232f2aa8b40d86a85a119cd38
Adrian <ad.penchev@gmail.com>
2022-10-10 20:04:30 -0400

    Inherit Classes multiple, using Map

---
+++ part09-Part09_10.OnlineShop/src/main/java/ShoppingCart.java
@@ -0,0 +1,35 @@
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+
+public class ShoppingCart {
+    private Map<String,Item> cart=new HashMap<>();
+    
+    public void add(String product, int price){
+        if(cart.containsKey(product)){
+           cart.get(product).increaseQuantity();;
+        }else{
+            Item newItem=new Item(product,1,price);
+            cart.put(product,newItem);
+        }
+    }
+    public int price(){
+        int totalPrice=0;
+        Collection<Item> values = cart.values();
+        for(Item prices: values){
+            totalPrice+=prices.price();
+        }
+        return totalPrice;
+    }
+    public void print(){
+        Set<String> keys = cart.keySet();
+        Collection<String> keyCollection = keys;
+        for(String keyCycle: keyCollection){
+            System.out.println(cart.toString());
+        }
+        
+    }
+}
---
+++ part09-Part09_10.OnlineShop/src/main/java/Warehouse.java
@@ -0,0 +1,55 @@
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+
+public class Warehouse {
+    private Map<String, Integer> prices=new HashMap<>();;
+    private Map<String, Integer> stocks=new HashMap<>();;
+    
+    public void addProduct(String product, int price, int stock){
+        prices.put(product,price);
+        stocks.put(product,stock);
+    }
+    
+    public int price(String product){
+        Set<String> keys = prices.keySet();
+        Collection<String> keyCollection = keys;
+        for(String key: keyCollection){
+            if(key.equals(product)){
+                return this.prices.get(product);
+            }
+        }
+        return -99;
+    }
+    public int stock(String product){
+        Set<String> keys = stocks.keySet();
+        Collection<String> keyCollection = keys;
+        for(String key: keyCollection){
+            if(key.equals(product)){
+                return this.stocks.get(product);
+            }
+        }
+        return 0;
+    }
+    public boolean take(String product){
+        if(stocks.containsKey(product)){
+            int productStock=stocks.get(product);
+            if(productStock==0){
+                productStock++;
+            }
+            stocks.put(product,productStock-1);
+            if(stocks.get(product)>0){
+                return true;
+            }
+        }
+        return false;
+    }
+    public Set<String> products(){
+        Set<String> keys = stocks.keySet();
+        return keys;
+    }
+    
+}
