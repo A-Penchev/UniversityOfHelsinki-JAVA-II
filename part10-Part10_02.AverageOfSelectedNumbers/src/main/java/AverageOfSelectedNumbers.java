c729e04bcbceb6790972b024a5a811e1c04d3b27
Adrian <ad.penchev@gmail.com>
2022-10-12 19:50:24 -0400

    Using streamer to filter out positve/negative numbers from list

--- part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -1,12 +1,41 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AverageOfSelectedNumbers {
 
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
+        List<String> inputs=new ArrayList<>();
+        
+        System.out.println("Input numbers, type \"end\" to stop.");
+        while(true){
+            String input=scanner.nextLine();
+            if(input.equals("end")){
+                break;
+            }
+            inputs.add(input); 
+        }
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+        String nOrP=scanner.nextLine();
+       if(nOrP.equals("p")){
+        double average = inputs.stream()
+            .mapToInt(s -> Integer.valueOf(s))
+            .filter(number->number>=0)
+            .average()
+            .getAsDouble();
+        System.out.println("average of the numbers: "+average);
+        }
+       if(nOrP.equals("n")){
+        double average = inputs.stream()
+            .mapToInt(s -> Integer.valueOf(s))
+            .filter(number->number<0)
+            .average()
+            .getAsDouble();
+        System.out.println("average of the numbers: "+average);
+        }
 
     }
 }
