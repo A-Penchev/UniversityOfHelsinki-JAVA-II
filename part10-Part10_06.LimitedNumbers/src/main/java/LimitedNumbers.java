14e9befad5d7f725485bb391850af0d10a730f9a
Adrian <ad.penchev@gmail.com>
2022-10-13 19:22:09 -0400

    Using streamer to print numbers in arraylist that are between 1-5

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -1,11 +1,26 @@
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        ArrayList<Integer> inputs=new ArrayList<>();
+            while(true){
+                int input=Integer.valueOf(scanner.nextLine());
+                if(input<0){
+                    break;
+                }
+                inputs.add(input);
+            }
+        
+        inputs.stream()
+                .filter(number->number<=5&&number>=1)
+                .collect(Collectors.toCollection(ArrayList::new))
+                .forEach(number->System.out.println(number));
+    
     }
 }
