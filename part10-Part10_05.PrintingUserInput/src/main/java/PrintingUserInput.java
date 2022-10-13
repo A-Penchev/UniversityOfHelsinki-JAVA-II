ecfc924d4e0f5c2cd8bfadf5b222fba81f9d3e61
Adrian <ad.penchev@gmail.com>
2022-10-13 18:46:47 -0400

    Using streamer to output arraylist line by line

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -6,6 +6,20 @@ public class PrintingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<String> inputs=new ArrayList<>();
+        
+        while(true){
+            String input=scanner.nextLine();
+            if(input.isEmpty()){
+                break;
+            }
+            inputs.add(input);
+        }
+        
+        String combined=inputs.stream()
+                .reduce("",(previousString,word)->previousString+word+"\n");
+        System.out.println(combined);
+        
+        //inputs.stream().forEach(s -> System.out.println(s));
     }
 }
