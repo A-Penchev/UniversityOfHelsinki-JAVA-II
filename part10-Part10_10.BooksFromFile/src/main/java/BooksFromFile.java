e748467f5fefe5018a5cc484a6a0353afe11f66c
Adrian <ad.penchev@gmail.com>
2022-10-18 18:50:12 -0400

    Using streamer to print off file line by line, seperating the file by commas and adding the parts to an object class list.

--- part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
+++ part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
@@ -1,9 +1,12 @@
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class BooksFromFile {
@@ -12,6 +15,19 @@ public class BooksFromFile {
         Scanner scanner = new Scanner(System.in);
         // test your method here
 
+    }
+    public static List<Book> readBooks(String file){
+        List<Book> books = new ArrayList<>();
+        try{
+            Files.lines(Paths.get(file))
+                    .map(rows->rows.split(","))
+                    .map(parts->new Book(parts[0],Integer.valueOf(parts[1]),Integer.valueOf(parts[2]),parts[3]))
+                    .forEach(book->books.add(book));
+                
+                } catch (IOException ex) {
+            Logger.getLogger(BooksFromFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return books;
     }
 
 }
