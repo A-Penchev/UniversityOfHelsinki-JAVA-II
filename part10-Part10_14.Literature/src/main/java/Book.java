6e5438c347ece9474933fbc50e6e6e1e939ad619
Adrian <ad.penchev@gmail.com>
2022-10-19 19:42:14 -0400

    Using Comparator to sort list multiple with multiple values.

---
+++ part10-Part10_14.Literature/src/main/java/Book.java
@@ -0,0 +1,19 @@
+
+public class Book {
+    private String name;
+    private int ageRec;
+    
+    public Book(String name,int ageRec){
+        this.name=name;
+        this.ageRec=ageRec;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public int getAgeRec(){
+        return this.ageRec;
+    }
+    
+}
