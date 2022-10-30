6482074afe2e5116e59f6bc9eaf0ff3b4d1675c5
Adrian <ad.penchev@gmail.com>
2022-10-30 17:51:37 -0400

    Using Stringbuilder to append values given by matrix to a string, linebreak between each value

--- part12-Part12_09.ArrayAsAString/src/main/java/Program.java
+++ part12-Part12_09.ArrayAsAString/src/main/java/Program.java
@@ -1,8 +1,35 @@
 
+import java.util.Arrays;
+
+
 public class Program {
 
     public static void main(String[] args) {
-        // Test your method here
-    }
+    /***
+    int[][] matrix = {
+    {3, 2, 7, 6},
+    {2, 4, 1, 0},
+    {3, 2, 1, 0}
+};
 
+System.out.println(arrayAsString(matrix));
+* int rows = 2;
+int columns = 3;
+int[][] matrix = new int[rows][columns];
+matrix[0][1] = 5;
+matrix[1][0] = 3;
+matrix[1][2] = 7;
+System.out.println(arrayAsString(matrix));***/
+    }
+    public static String arrayAsString(int[][] array){
+        StringBuilder outPut=new StringBuilder();
+        for (int row = 0; row < array.length; row++) {
+            for (int column = 0; column < array[row].length; column++) {
+                int value = array[row][column];
+                outPut.append(value);
+                }
+                outPut.append('\n'); //outPut.append(System.lineSeparator());
+            }
+        return outPut.toString();
+    }
 }
