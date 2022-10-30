ac3df0d3d30596fdb5c4c70a1cf59a8ea099d00a
Adrian <ad.penchev@gmail.com>
2022-10-30 16:27:52 -0400

    Random number generator, given index

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,15 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("How many random numbers should be printed?");
+        int input=Integer.valueOf(scanner.nextLine());
+        
+        Random ladyLuck = new Random();
+        for(int i=0; i<input;i++){
+            int randomNumber=ladyLuck.nextInt(11);
+            System.out.println(randomNumber);
+        }
     }
 
 }
