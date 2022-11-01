0c0a883b208db7c0f0f4206fc790f07c3aa07df0
Adrian <ad.penchev@gmail.com>
2022-11-01 18:54:37 -0400

    GUI setup, button/textfield

--- part13-Part13_03.ButtonAndTextField/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
+++ part13-Part13_03.ButtonAndTextField/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
@@ -1,11 +1,32 @@
 package buttonandtextfield;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.Scene;
 
-public class ButtonAndTextFieldApplication {
+public class ButtonAndTextFieldApplication extends Application{
 
+    @Override
+    public void start(Stage stage) {
+        TextField text = new TextField();
+        Button button = new Button("Update");
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+        VBox components = new VBox();
+        components.getChildren().addAll(text, button);
+
+        Scene scene = new Scene(components,200,100);
+
+        stage.setScene(scene);
+        stage.show();
     }
-
+    
+    public static void main(String[] args) {
+        launch(ButtonAndTextFieldApplication.class);
+    }
 }
+
