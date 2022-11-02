46b90792daf1c8ad3abd4fc47d083e63f9e3ee7e
Adrian <ad.penchev@gmail.com>
2022-11-01 20:31:26 -0400

    GUI setup, BorderPane with HBox underneath

--- part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
+++ part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,39 @@
 package textstatistics;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
 
-public class TextStatisticsApplication {
+public class TextStatisticsApplication extends Application {
 
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    @Override
+    public void start(Stage stage) {
+        BorderPane layout = new BorderPane();
+        HBox components=new HBox();
+ 
+        layout.setCenter(new TextArea(""));
+        
+        components.getChildren().add(new Label("Letters: 0"));
+        components.getChildren().add(new Label("Words: 0"));
+        components.getChildren().add(new Label("The longest word is:"));
+        
+        layout.setBottom(components);
+        
+        Scene scene = new Scene(layout);
+
+        stage.setScene(scene);
+        stage.show();
     }
-
+    
+    public static void main(String[] args) {
+        launch(TextStatisticsApplication.class);
+    }
 }
