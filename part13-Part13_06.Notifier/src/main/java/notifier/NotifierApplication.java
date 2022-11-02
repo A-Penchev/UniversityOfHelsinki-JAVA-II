54b1197b1ae37dc437532fb784948d7feb1b2c00
Adrian <ad.penchev@gmail.com>
2022-11-02 19:48:35 -0400

    GUI setup, VBox, textfield and button that copies textfield input to label

--- part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
+++ part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
@@ -1,11 +1,41 @@
 package notifier;
 
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class NotifierApplication {
+
+public class NotifierApplication extends Application {
 
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    @Override
+    public void start(Stage stage) {
+        
+        VBox components=new VBox();
+        TextField top = new TextField();
+        Button middle = new Button("Update");
+        Label bottom=new Label("");
+        
+        middle.setOnAction((event) -> {
+        bottom.setText(top.getText());});
+        
+        components.getChildren().addAll(top,middle,bottom);
+  
+        Scene scene = new Scene(components);
+
+        stage.setScene(scene);
+        stage.show();
     }
-
+    
+    public static void main(String[] args) {
+        launch(NotifierApplication.class);
+    }
 }
