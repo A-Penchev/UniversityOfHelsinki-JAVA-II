66f895bb32e7e22fad51038f1aa58aa25ac4689d
Adrian <ad.penchev@gmail.com>
2022-11-05 12:20:57 -0400

    GUI setup, use button to change to copy input in textfield to next page

--- part13-Part13_10.Greeter/src/main/java/application/GreeterApplication.java
+++ part13-Part13_10.Greeter/src/main/java/application/GreeterApplication.java
@@ -1,9 +1,47 @@
 package application;
 
-public class GreeterApplication {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class GreeterApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hellow world! :3");
+        launch(GreeterApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) throws Exception {
+        VBox firstView=new VBox();
+        
+        Label welcome=new Label("Enter your name and start.");
+        TextField textField=new TextField();
+        Button startButton=new Button("Start");
+        
+        firstView.getChildren().addAll(welcome,textField,startButton);
+        //first scene
+        
+        BorderPane secondView=new BorderPane();
+        Label name=new Label();
+        
+        secondView.setCenter(name);
+        //second scene
+        
+        Scene first = new Scene(firstView);
+        Scene second = new Scene(secondView);
+        
+        startButton.setOnAction((event) -> {
+          name.setText("Welcome "+textField.getText()+"!");
+          window.setScene(second);
+        });
+        
+        window.setScene(first);
+        window.show();
     }
 }
